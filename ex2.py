cbf130c7ddd7f032e290567a88db50010e9843d5
mlope579 <44369570+mlope579@users.noreply.github.com>
2018-10-22 19:16:13 -0400

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,5 @@
+# A comment, this is so you can read your program later.
+#anything after the # is ignored by python.
+print "I could have code like this" # and the comment after is ignored
+# you can also use a comment to "disable" or comment out a piece of code:
+print "This will run."
