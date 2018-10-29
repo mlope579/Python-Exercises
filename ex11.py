b7fc6409c017effc6b41155d471f07c1f0dc1b89
mlope579 <44369570+mlope579@users.noreply.github.com>
2018-10-29 18:23:38 -0400

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = input(25)
+print("How tall are you?", end=' ')
+height = input(5'6')
+print("How much do you weigh?", end=' ')
+weight = input(140)
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
